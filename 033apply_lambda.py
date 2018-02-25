51d33965dcfe99e516e05a0381e2f37de30a70d7
Apocrypse <lucastang1994@gmail.com>
2018-02-25 14:15:15 -0500

    Initial commit

---
+++ 033apply_lambda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('employees.csv')
+
+get_last_name = lambda name: name.split()[-1]
+
+df['last_name'] = df.name.apply(get_last_name)
+
+print df
---
+++ 036project_petal_power_inventory.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+inventory = pd.read_csv('inventory.csv')
+
+staten_island = inventory.head(10)
+product_request = staten_island.product_description
+
+seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
+
+stock = lambda quantity: True if quantity > 0 else False
+inventory['in_stock'] = inventory.quantity.apply(stock)
+
+value = lambda row: row.price * row.quantity
+inventory['total_value'] = inventory.apply(value, axis=1)
+
+combine = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+inventory['full_description'] = inventory.apply(combine, axis=1)
+
+print inventory.head(10)
