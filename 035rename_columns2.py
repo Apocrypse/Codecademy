ae35eeedb8a289313980b95269bf80cb4878ba57
Apocrypse <lucastang1994@gmail.com>
2018-03-22 15:06:20 -0400

    Initial commit

--- 035rename_columns2.py
+++ 035rename_columns2.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('imdb.csv')
 
-df.rename(columns={
-  'name': 'movie_title'},
-  inplace = True)
+df.rename(columns={'name': 'movie_title'}, inplace = True)
 
 print(df)
--- 045inner_merge2.py
+++ 045inner_merge2.py
@@ -6,8 +6,7 @@ targets = pd.read_csv('targets.csv')
 
 men_women = pd.read_csv('men_women_sales.csv')
 
-all_data = sales.merge(targets)\
-	.merge(men_women)
+all_data = sales.merge(targets).merge(men_women)
 
 results = all_data[(all_data.revenue > all_data.target) & (all_data.women > all_data.men)]
 print(results)
