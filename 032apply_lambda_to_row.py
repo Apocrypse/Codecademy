ab432e37f23a11e4c94392186bbcb24427d6b3e3
Apocrypse <lucastang1994@gmail.com>
2018-03-23 18:45:54 -0400

    Initial commit

--- 032apply_lambda_to_row.py
+++ 032apply_lambda_to_row.py
@@ -6,6 +6,6 @@ total_earned = lambda row: row.hourly_wage * 40 + 1.5 * row.hourly_wage * (row.h
 	                       if row.hours_worked > 40 \
                            else row.hourly_wage * row.hours_worked
 
-df['total_earned'] = df.apply(total_earned, axis = 1)
+df['total_earned'] = df.apply(total_earned, axis=1)
 
 print(df)
