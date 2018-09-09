09776cebba67affea92bcb9ade4bfc51f8a7b2fa
Apocrypse <lucastang1994@gmail.com>
2018-09-08 20:06:37 -0400

    Initial commit

--- 038aggregate1.py
+++ 038aggregate1.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 orders = pd.read_csv('orders.csv')
+print(orders.head(10))
 
-pricey_shoes = orders.groupby('shoe_type').price.max()
-print(pricey_shoes)
-print(type(pricey_shoes))
+most_expensive = orders.price.max()
+
+num_colors = orders.shoe_color.nunique()
